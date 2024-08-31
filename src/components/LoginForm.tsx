93ec715e2fdd365969e8291a68428ef0c93adf7b
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-08-31 12:06:23 -0300

    feat: initial LoginForm

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const schema = z.object({
+  email: z.string()
+    .min(1, 'E-mail é obrigatório')
+    .email('Informe um e-mail válido'),
+  password: z.string()
+    .min(6, 'Senha deve conter no minímo 06 digítos'),
+});
+
+export default function LoginForm() {
+  const {
+    handleSubmit: hookFormHandleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<z.infer<typeof schema>>({ resolver: zodResolver(schema) });
+
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    try {
+      console.log(data)
+    } catch (error) {
+      //toast.error('Ocorreu um erro ao realizar o login!')
+    }
+  });
+
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Realize seu login</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">E-mail</Label>
+              <Input
+                id="email"
+                type='email'
+                placeholder="E-mail"
+                error={errors.email?.message}
+                {...register('email')}
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Senha</Label>
+              <Input
+                id="password"
+                placeholder="Senha"
+                error={errors.password?.message}
+                {...register('password')}
+              />
+            </div>
+
+            <div className="flex flex-col space-y-1.5">
+              <Button type="submit">Login</Button>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
