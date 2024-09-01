caeb0a60a3ef44ce89b71576effac885763c23cd
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-09-01 10:27:29 -0300

    feat: layout default for signin and signup

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,13 @@
-import LoginForm from "@/components/LoginForm";
+import { ReactElement } from "react";
 
-export default async function SignInPage() {
+export default async function AuthLayout({ children }: { children: ReactElement }) {
   return (
     <div className="h-screen grid grid-cols-1 md:grid-cols-2">
       <div className="bg-muted hidden md:flex items-end justify-end p-4">
         <h1 className="font-bold">Influencer App</h1>
       </div>
       <div className="flex items-center justify-center p-4">
-        <LoginForm />
+        {children}
       </div>
     </div>
   );
