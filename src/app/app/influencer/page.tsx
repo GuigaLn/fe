5a1c9c94509436fe4c33290080f41b5e4840d9cc
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-09-02 20:28:44 -0300

    feat: InfluencerPage

--- src/app/app/influencer/page.tsx
+++ src/app/app/influencer/page.tsx
@@ -1,7 +1,12 @@
+import { influencerService } from "@/app/services/influencer";
+import { DataTableInfluencers } from "./components/datatable-influencers";
+
 export default async function InfluencerPage() {
+  const influencer = await influencerService.findAll();
+
   return (
     <div>
-      influencers
+      <DataTableInfluencers influencer={[]} />
     </div>
   );
 }
